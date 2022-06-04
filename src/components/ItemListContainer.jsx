0cbc14b55284dfa000db0292fa86202364dc9294
coliTNE <tinellijuan7@gmail.com>
2022-06-04 02:46:36 -0300

    fixed initial props

--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -3,6 +3,6 @@ import ItemCount from './ItemCount';
 
 export default function ItemListContainer() {
   return (
-    <div><ItemCount stock={10}/></div>
+    <div><ItemCount stock={10} initial={1}/></div>
   )
 }
