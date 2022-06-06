dac86d9a472077de0b69aae0a5ad73e321d67a4b
coliTNE <tinellijuan7@gmail.com>
2022-06-06 18:10:47 -0300

    improve shoeCards Css

--- src/components/Helpers/Items/Item.jsx
+++ src/components/Helpers/Items/Item.jsx
@@ -1,19 +1,32 @@
 import React from "react";
 
 export default function Item({ product }) {
-  const { brand, title, type, description, price, pictureUrl } = product;
+  const {
+    brand,
+    title,
+    type,
+    description,
+    price,
+    pictureUrl,
+    secondPictureurl,
+  } = product;
   return (
     <div className="main__shoeCard">
       <div className="shoeCard__imgContainer">
+        <img src={title} alt={`${brand} logo`} className="shoeCard__brand" />
         <img
           src={pictureUrl}
           alt={`zapatilla deportiva ${brand}`}
           className="shoeCard__img"
         />
-        <img src={title} alt={`${brand} logo`} className="shoeCard__brand" />
+        <img
+          src={secondPictureurl}
+          alt={`zapatilla deportiva ${brand}`}
+          className="shoeCard__secondaryImg"
+        />
       </div>
       <div className="shoeCard__dataContainer">
-        <p className="shoeCard__type">{type}</p>
+        <span className="shoeCard__type">{type}</span>
         <p className="shoeCard__descrip">{description}</p>
         <h3 className="shoeCard__price">{price}</h3>
       </div>
--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -16,9 +16,10 @@ export default function ItemListContainer() {
       price: "$11.990",
       pictureUrl:
         "https://cdn.solodeportes.com.ar/media/catalog/product/cache/3cb7d75bc2a65211451e92c5381048e9/z/a/zapatillas-running-asics-patriot-12-noosa-gris-13001011b264021-1.jpg",
+      secondPictureurl: "https://cdn.solodeportes.com.ar/media/catalog/product/cache/3cb7d75bc2a65211451e92c5381048e9/z/a/zapatillas-running-asics-patriot-12-noosa-gris-13001011b264021-2.jpg",
     },
     {
-      id: 1,
+      id: 2,
       brand: "asics",
       title: AsicsLogo,
       type: "Running",
@@ -26,9 +27,10 @@ export default function ItemListContainer() {
       price: "$33.490",
       pictureUrl:
         "https://cdn.solodeportes.com.ar/media/catalog/product/cache/7c4f9b393f0b8cb75f2b74fe5e9e52aa/z/a/zapatillas-running-asics-gel-nimbus-24-azul-39551509-13001011b359003-1.jpg",
+      secondPictureurl: "https://cdn.solodeportes.com.ar/media/catalog/product/cache/7c4f9b393f0b8cb75f2b74fe5e9e52aa/z/a/zapatillas-running-asics-gel-nimbus-24-azul-39551509-13001011b359003-2.jpg",
     },
     {
-      id: 1,
+      id: 3,
       brand: "adidas",
       title: AdidasLogo,
       type: "Running",
@@ -36,19 +38,21 @@ export default function ItemListContainer() {
       price: "$12.499",
       pictureUrl:
         "https://cdn.solodeportes.com.ar/media/catalog/product/cache/7c4f9b393f0b8cb75f2b74fe5e9e52aa/z/a/zapatillas-running-adidas-galaxy-5-gris-69496796-100010h04594001-1.jpg",
+      secondPictureurl: "https://cdn.solodeportes.com.ar/media/catalog/product/cache/3cb7d75bc2a65211451e92c5381048e9/z/a/zapatillas-running-adidas-galaxy-5-gris-69496796-100010h04594001-2.jpg",
     },
     {
-      id: 1,
+      id: 4,
       brand: "nike",
       title: NikeLogo,
       type: "Moda",
-      description: "NIKE COURT ROYALE AC MARRON",
-      price: "$12.099",
+      description: "NIKE SB CHARGE SUEDE AZUL",
+      price: "$13.909",
       pictureUrl:
-        "https://cdn.solodeportes.com.ar/media/catalog/product/cache/3cb7d75bc2a65211451e92c5381048e9/5/1/510010bq4222200-1.jpg",
+        "https://cdn.solodeportes.com.ar/media/catalog/product/cache/7c4f9b393f0b8cb75f2b74fe5e9e52aa/z/a/zapatillas-nike-sb-charge-suede-azul-510010ct3463401-1.jpg",
+      secondPictureurl: "https://cdn.solodeportes.com.ar/media/catalog/product/cache/3cb7d75bc2a65211451e92c5381048e9/z/a/zapatillas-nike-sb-charge-suede-azul-510010ct3463401-2.jpg",
     },
     {
-      id: 1,
+      id: 5,
       brand: "adidas",
       title: AdidasLogo,
       type: "Moda",
@@ -56,6 +60,7 @@ export default function ItemListContainer() {
       price: "$12.999",
       pictureUrl:
         "https://cdn.solodeportes.com.ar/media/catalog/product/cache/7c4f9b393f0b8cb75f2b74fe5e9e52aa/z/a/zapatilla-adidas-grand-court-base-blanca-97487218-100010ee7904001-1.jpg",
+      secondPictureurl: "https://cdn.solodeportes.com.ar/media/catalog/product/cache/3cb7d75bc2a65211451e92c5381048e9/z/a/zapatilla-adidas-grand-court-base-blanca-97487218-100010ee7904001-2.jpg",
     },
   ];
 
@@ -85,50 +90,3 @@ export default function ItemListContainer() {
     </>
   );
 }
-
-// const Prod = [
-//   {
-//     id: 1,
-//     name: "Escuadra",
-//     description: "Articulo escolar",
-//     stock: 10,
-//   },
-//   {
-//     id: 2,
-//     name: "Regla",
-//     description: "Articulo escolar",
-//     stock: 20,
-//   },
-//   {
-//     id: 3,
-//     name: "Goma",
-//     description: "Articulo escolar",
-//     stock: 30,
-//   },
-// ];
-
-// export default function Desafip() {
-//   const [productos, setProductos] = useState();
-
-//   const carrito = new Promise((res, rej) => {
-//     setTimeout(() => {
-//       res(Prod);
-//     }, 3000);
-//   });
-
-//   carrito.then((result) => {
-//     setProductos(result);
-//   });
-
-//   return (
-//     <div>
-//       {productos &&
-//         productos.map((item) => (
-//           <div>
-//             <p>{item.name}</p>
-//             <p>{item.stock}</p>
-//           </div>
-//         ))}
-//     </div>
-//   );
-// }
